bac24298f40c8b3020679df1cdb4b50331ec77fd
Max Xu <maxxu05@gmail.com>
2022-09-06 12:51:13 -0400

    refactor

--- openreviews_parsing.py
+++ openreviews_parsing.py
@@ -1,6 +1,9 @@
 import openreview
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+from datetime import datetime
+import os
 
 # https://openreview-py.readthedocs.io/en/latest/ 
 # https://readthedocs.org/projects/openreview-py-dm-branch/downloads/pdf/latest/
@@ -14,10 +17,16 @@ papers = {}
 # print(client.get_group(id='venues').members)
 submissions = client.get_all_notes(invitation="NeurIPS.cc/2022/Track/Datasets_and_Benchmarks/-/Submission", details='directReplies')
 # import pdb; pdb.set_trace()
+listofreviewers = set()
 for submission in submissions:
     reviews = []
     for review in submission.details['directReplies']:
         if 'rating' in review['content']:
+            if review["signatures"][0][-4:] not in listofreviewers:
+                listofreviewers.add(review["signatures"][0][-4:])
+            else:
+                print(review["signatures"][0][-4:])
+                import pdb; pdb.set_trace()
             rating = int(review['content']['rating'].split(':')[0])
             confidence = int(review['content']['confidence'].split(':')[0])
             aTup = rating,confidence
@@ -50,11 +59,32 @@ print(f"Standard Deviation of Mean Paper Rating: {overallStdev}")
 print(f"Total Nonwithdrawn Papers: {len(allRatingsMeans)}")
 
 # Visualize results
-plt.hist(allRatingsMeans, bins=100)
+fig, ax = plt.subplots(figsize=(20,10))
+
+counts, bins, patches = ax.hist(allRatingsMeans, bins=50)
+# Set the ticks to be at the edges of the bins.
+ax.set_xticks(bins)
+# Set the xaxis's tick labels to be formatted with 1 decimal place...
+ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+
+# Label the raw counts and the percentages below the x-axis...
+bin_centers = 0.5 * np.diff(bins) + bins[:-1]
+for count, x in zip(counts, bin_centers):
+    # Label the raw counts
+    ax.annotate(str(int(count)), xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0, -18), textcoords='offset points', va='top', ha='center')
+
+    # Label the percentages
+    percent = '%0.0f%%' % (100 * float(count) / counts.sum())
+    ax.annotate(percent, xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0, -32), textcoords='offset points', va='top', ha='center')
+
 plt.show(block=True)
 
 # Write to spreadsheet
-handle = open('openreview_ratings.csv', 'w')
+if not os.path.exists("csvs"):
+    os.makedirs("csvs")
+handle = open(f"csvs/openreview_ratings_{datetime.today().strftime('%Y_%m_%d')}.csv", 'w')
 handle.write("Title,Mean,Median,StDev," + "Rating,Confidence,"*8 + "\n")
 for title in papers:
     stuff = str(papers[title])
